f969acfc18ce824638a10bb15b9904f0594d8019
Septian Hari <septian@infoflow.co.id>
2019-01-17 23:04:27 +0700

    Add pygame

--- component/Arena.py
+++ component/Arena.py
@@ -1,6 +1,9 @@
 from .Coordinate import Coordinate
+import pygame
 
 class Arena:
+    __PIXEL_UNIT = 36
+    __FONT_SIZE = 0
     def __init__(self, field, red_side, red_side_location, blue_side, blue_side_location):
         self.field = field
         self.robots = [red_side, blue_side]
@@ -19,6 +22,11 @@ class Arena:
         }
 
         self.status = "IN-PROGRESS"
+        pygame.init()
+        self.top_pad = self.__FONT_SIZE * 2
+        self.screen = self.__create_screen()
+        self.clock = pygame.time.Clock()
+        self.__init_image()
 
     def game_in_progress(self):
         return "IN-PROGRESS" == self.status
@@ -59,15 +67,24 @@ class Arena:
             0: "o",
             1: "x",
         }
+        screen = self.screen
         for y in range(height - 1, -1, -1):
             for x in range(0, width):
                 if red_location.x == x and red_location.y == y:
+                    circle_black_pix = self.__pos_to_pix(red_location)
+                    screen.blit(self.circle_black, circle_black_pix) # draw black circle to next screen
                     print(1, end=' ')
                 elif blue_location.x == x and blue_location.y == y:
+                    circle_white_pix = self.__pos_to_pix(blue_location)
+                    screen.blit(self.circle_white, circle_white_pix) # draw black circle to next screen
                     print(2, end=' ')
                 else:
+                    tile_pix = self.__pos_to_pix(Coordinate(x,y))
+                    screen.blit(self.tile, tile_pix) # draw black circle to next screen
                     print(layout_mapping[layout[x][y]], end=' ')
             print()
+        pygame.display.flip()
+        self.clock.tick(120)
 
 
 
@@ -220,3 +237,29 @@ class Arena:
     def __penalty(self, robot):
         print("penalized")
         self.penalty_mapping[robot] = True
+
+    def __init_image(self):
+        self.circle_black = pygame.image.load("images/circle_black.png")
+        self.circle_white = pygame.image.load("images/circle_white.png")
+        self.tile = pygame.image.load("images/tile.png")
+
+    def __pos_to_pix(self, coordinate):
+        return (
+            coordinate.x * self.__PIXEL_UNIT,
+            coordinate.y * self.__PIXEL_UNIT + self.top_pad
+        )
+
+    def __create_screen(self):
+        (width, height) = self.field.get_dimension()
+        return pygame.display.set_mode(
+            (
+                self.__PIXEL_UNIT * width,
+                self.__PIXEL_UNIT * height + self.top_pad
+            )
+        )
+
+    def __pos_to_pix(self, coordinate):
+        return (
+            coordinate.x * self.__PIXEL_UNIT,
+            coordinate.y * self.__PIXEL_UNIT + self.top_pad
+        )
